2c429627480e76df3b3e173e8d9620cf629347fc
William Gotterer <william.gotterer@gmail.com>
2022-01-19 21:04:05 -0500

    user cards

--- src/components/MainScreen.js
+++ src/components/MainScreen.js
@@ -1,24 +1,23 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import UserCard from './UserCard'
 
  function MainScreen() {
 
     const [userInput, setUserInput] = useState('')
     const [userList, setUserList] = useState()
-
-    function displayUsers (){
+   
+    useEffect(() => {
+        
         fetch(`https://api.github.com/search/users?q=${userInput}&?page=1&per_page=10`)
         .then(r => r.json()
         .then(userInfo => setUserList(userInfo))
          )
-    }
-   
+    }, [userInput])
 
     function handleOnChange(e){
         setUserInput(e.target.value)
-        displayUsers()
     }
-    console.log(userList)
+    
 
     return  (
         <div>
@@ -30,14 +29,16 @@ import UserCard from './UserCard'
                 name = "search"
                 onChange = {handleOnChange}
                 type = "search"
+                placeholder="search by name"
                 />
                 </label>
             </form>
         { userList && userList["items"] ? 
         userList["items"].map((oneUser)=> <UserCard
         avatar={oneUser.avatar_url}
+        username={oneUser.login}
         
-        /> )  : "Loading" }
+        /> )  : null }
         </div>
     )
 }
--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -1,11 +1,34 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 
- function UserCards({avatar}) {
-    return (
+ function UserCards({avatar, username}) {
+
+    const [userInfo, setUserInfo] = useState()
+
+
+    useEffect(() => {
+        if (username){
+        
+            fetch(`https://api.github.com/users/${username}`)
+        .then(r => r.json())
+        .then((repoData) => setUserInfo(repoData))
+        
+    }else{
+        return null
+    }
+    }, [username])
+   
+       
+        console.log(userInfo)
+  
+
+
+    return userInfo ? (
         <div>
+            <h2>{username}</h2>
             <img height="100" width="100" src={avatar} />
+            <h3>Public Repos: {userInfo.public_repos}</h3>
         </div>
-    )
+    ) : null
 }
 
 export default UserCards
